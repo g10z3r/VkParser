cc3635611c8a589ddc370b3d8d7822839a47d557
egorsobolev2000 <web-disain@mail.ua>
2020-03-23 15:22:40 +0300

    Упростил код, пофиксил баг с парсингом статьи без цитаты

--- article_builder.py
+++ article_builder.py
@@ -10,31 +10,6 @@ class ArticleParser():
         self.article = article
         
         self.tag_list = ['h1', 'blockquote']
-    
-    def __pattern__(self, pattern):
-        # Конкретный строитель третьего строителя
-        """Внутренние использование простого паттерна для поиска фрагмента нужного"""
-        result = re.findall(pattern, str(self.article))
-        result = str(self.article).replace(result[0], '')
-        return result
-    
-    def __patternCreate__(self, tag):
-        # Конкретный строитель третьего строителя
-        """Создание простого паттерна для поиска фрагмента нужного"""
-        pattern = re.compile(r'<'+ tag + r'.*>.*<\/'+ tag +r'>')
-        return pattern
-
-    def simple_replacement_cleaning(self):
-        """Простая замена найденных по совпадениям нужных
-        фрагментов (если они существуют)"""
-        for tag in self.tag_list:                      
-            if str(self.article.find(tag)) in str(self.article):
-                for t in self.tag_list:                                
-                    self.article = self.__pattern__(self.__patternCreate__(t))
-                return self.article            
-            else:
-                return self.article   
-
         
     def re_replacement_cleaning(self, regex, regex_key):
         # Основной строитель
